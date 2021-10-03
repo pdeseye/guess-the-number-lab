5d48fe1f3fbe81b3a310dd8342fb6eb31d853bca
Payal Desai <payal@idesai.com>
2021-10-02 22:52:00 -0400

    solved

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
+
+
 const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+
   getGuess(){
-    let currentGuess = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
-    console.log(currentGuess)
+    let userInput = parseInt( prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`), )
+    while (userInput > this.bigggestNum || userInput < this.smallestNum || isNaN (userInput) || userInput > this.biggestNum + isNaN(userInput) || userInput < this.smallestNum + isNaN(userInput)) {
+      userInput = parseInt (prompt (`Try again! The guess must be between ${this.biggestNum} and ${this.smallestNum}. Please guess again.`))
+    } console.log(userInput)
   },
+
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+  },
+
+  // access getGuess in play function
+    // build the while loop
+    // return value of getGuess to previousGuesses
+    // print (as in display on the actual page of the html) previousGuess not as an array
+
+    
+  // do {
+  //   getGuess = getGuess + 1
+  // } while (userInput != this.secretNum)
 }
 game.getGuess()
+
+
+
+// Goals for do while loop:
+    // while userInput != secretNum
+    // check if userInput > secretNum
+    // return a prompt that states the guess (parseInt(userInput)) was too high
+    // push.userInput(previousGuesses[])
+    // print previousGuesses (previousGuesses.toString())
+    // Else
+    // check if user input is < secretNum
+    // return a prompt that guess is too low
+    // let previousGuesses []
+    //
+    // else if userInput = secretNum break
